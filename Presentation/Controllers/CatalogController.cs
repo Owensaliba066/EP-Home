7db66cd1be748155e3d34ffc3ad0377a221e73f3
Owen Saliba <owen.saliba.j95201@mcast.edu.mt>
2025-12-01 14:41:30 +0100

    Add catalog page with dynamic partial rendering

---
+++ Presentation/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAccess.Context;
+using Domain.Interfaces;
+using System.Linq;
+
+namespace Presentation.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CatalogController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            // Load restaurants + menu items
+            var restaurants = _db.Restaurants.ToList();
+            var menuItems = _db.MenuItems.ToList();
+
+            // Combine into a single polymorphic list
+            List<IItemValidating> items = new List<IItemValidating>();
+            items.AddRange(restaurants);
+            items.AddRange(menuItems);
+
+            return View(items);
+        }
+    }
+}
